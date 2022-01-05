8a8ff8e66d334efe5e1bc4489c4d13472f80a9e8
SonOfMagic <qq1324318532@gmail.com>
2022-01-05 21:57:59 +0800

    feat: init project

--- src/components/vue-relation-graph/index.js
+++ src/components/vue-relation-graph/index.js
@@ -1,5 +1,5 @@
 import RelationGraph from './index.vue'
-RelationGraph.install = function(Vue) {
+RelationGraph.install = function (Vue) {
     Vue.component('relation-graph', RelationGraph);
     Vue.component('seeks-relation-graph', RelationGraph);
 };
